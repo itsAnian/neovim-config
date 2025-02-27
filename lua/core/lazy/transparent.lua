4827c321c9da74a6b694739d75b1e79a13d44ca5
itsAnian <anian.seidl@gmail.com>
2025-02-27 15:14:50 +0100

    Add transparent plugin

---
+++ lua/core/lazy/transparent.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "xiyaowong/transparent.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("transparent").setup({
+                groups = {
+                    "Normal", "NormalNC", "Comment", "Constant", "Special", "Identifier",
+                    "Statement", "PreProc", "Type", "Underlined", "Todo", "String", "Function",
+                    "Conditional", "Repeat", "Operator", "Structure", "LineNr", "NonText",
+                    "SignColumn", "CursorLineNr", "EndOfBuffer",
+                },
+                extra_groups = {},
+                exclude_groups = {},
+            })
+        end
+    }
+}
