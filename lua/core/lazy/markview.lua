90907df609c9b5e367f5860a8af10527e1b9e3eb
itsAnian <anian.seidl@gmail.com>
2025-02-27 17:44:09 +0100

    Add markdown preview

---
+++ lua/core/lazy/markview.lua
@@ -0,0 +1,18 @@
+return {
+    'oxy2dev/markview.nvim',
+    lazy = true,
+    ft = { 'markdown' },
+    config = function()
+        require('markview').setup({
+            theme = 'dark',  -- Set theme to 'light' or 'dark'
+            auto_start = false,  -- Automatically start the preview on entering a markdown file
+        })
+        -- Add keymap to toggle splitview with leader+tm
+        vim.api.nvim_set_keymap(
+            'n',
+            '<leader>tm',
+            ':Markview splitToggle<CR>',
+            { noremap = true, silent = true }
+        )
+    end,
+}
