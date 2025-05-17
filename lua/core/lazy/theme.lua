1db8cb4bd7a7eaffa8119fe69ad1b5fa2cb31ec4
itsAnian <anian.seidl@gmail.com>
2025-05-17 10:16:43 +0200

    Change Theme to Everforest

---
+++ lua/core/lazy/theme.lua
@@ -0,0 +1,10 @@
+return {
+  "sainnhe/everforest",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.g.everforest_background = "soft"  -- oder 'medium', 'hard'
+    vim.g.everforest_enable_italic = 1
+    vim.cmd("colorscheme everforest")
+  end
+}
