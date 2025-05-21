e9c5c520cafe62e6a5cfcf9a337ca4fa9509c052
itsAnian <anian.seidl@gmail.com>
2025-05-21 08:08:01 +0200

    Add hotkey to :qa

--- lua/core/map.lua
+++ lua/core/map.lua
@@ -4,3 +4,4 @@ vim.keymap.set("n", "<leader>e", function() Snacks.explorer.open() end, { norema
 vim.keymap.set("n", "<leader>f", function() Snacks.picker.files() end, { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>q", ":qa<CR>")
