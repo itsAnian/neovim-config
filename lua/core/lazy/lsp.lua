5c18f0417d6bd4ab9f28e77ab20149cc88691c38
itsAnian <anian.seidl@gmail.com>
2025-08-01 13:10:31 +0200

    Add rust lsp

--- lua/core/lazy/lsp.lua
+++ lua/core/lazy/lsp.lua
@@ -64,6 +64,21 @@ return {
             },
         }
 
+        lspconfig.rust_analyzer.setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = {
+                ["rust-analyzer"] = {
+                    cargo = {
+                        allFeatures = true,
+                    },
+                    checkOnSave = {
+                        command = "clippy",
+                    },
+                },
+            },
+        })
+
         lspconfig.pyright.setup({
             on_attach = on_attach,
             settings = {
